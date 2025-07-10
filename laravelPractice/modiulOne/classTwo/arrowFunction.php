6b00bbb9cc2859ebc77b39acd7e9c7b11819351d
hasanwebdeb <hasanwebdeb@gmail.com>
2025-07-10 20:21:39 +0600

    file upload manually

---
+++ laravelPractice/modiulOne/classTwo/arrowFunction.php
@@ -0,0 +1,5 @@
+<?php
+$sum = fn($a, $b) => $a + $b;
+echo $sum(5, 2); // This will return 3
+// This is a simple arrow function that takes two parameters and returns their sum.
+// Arrow functions are a shorthand syntax for writing functions in PHP.
