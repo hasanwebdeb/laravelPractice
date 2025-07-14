0a85dfbbcada024f1a7b4318ff3a13f781462daa
hasanwebdeb <hasanwebdeb@gmail.com>
2025-07-15 00:13:23 +0600

    ceil and floor added

---
+++ modiulOne/classFour/math 1.php
@@ -0,0 +1,11 @@
+<?php
+$pieces = 4;
+$guests = 19;
+$numberOfPizzas = ceil($guests / $pieces);
+echo "Number of pizzas needed: $numberOfPizzas";
+
+
+$pieces = 4;
+$guests = 19;
+$numberOfPizza = floor($guests / $pieces);
+echo "Number of pizzas needed: $numberOfPizza";
