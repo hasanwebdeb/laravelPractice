4fa8ad298c0ded11f5a04471cc7a907e94c20806
hasanwebdeb <hasanwebdeb@gmail.com>
2025-07-19 17:22:17 +0600

    date 3 right update ok

---
+++ modiulOne/classFour/date3.php
@@ -0,0 +1,14 @@
+<?php
+$tanib = "2019/01/29";
+$tuba = "2022/11/10";
+
+$dOne = new DateTime($tanib);
+$dTwo = new DateTime($tuba);
+
+$diff = $dOne->diff ($dTwo);
+
+echo $diff->y ." Years " .$diff->m ." Month " .$diff->d ." Days ";
+
+//echo $diff->format("%y/%m/%d");
+
+echo $diff->format("%a");
