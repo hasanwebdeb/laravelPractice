e23dd32d5795c9e1c1fffbf491df5131ac9f4ca6
hasanwebdeb <hasanwebdeb@gmail.com>
2025-07-24 10:19:03 +0600

    File created successful

---
+++ modiulOne/classFive/createHtmlFile.php
@@ -0,0 +1,11 @@
+<?php
+$fileName = "modiulOne/classFive/index.html";
+$content = "!";
+file_put_contents($fileName,$content);
+
+$fileName = "modiulOne/classFive/index2.html";
+$content = "<h1>Create a html file</h1>";
+$file = fopen($fileName,"w");
+fwrite($file, $content);
+fclose($file);
+
